4474f9be229a434f8ae0146c081b08b426e0446e
Julien Brasseur <julien.brasseur@dwarf-labs.com>
2015-12-02 11:13:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,35 @@
-#!-*- coding: utf-8 -*-
-
-import os
 from setuptools import setup, find_packages
+import os
 
+version_file = os.path.join(os.path.dirname(__file__), 'timecode', '__VERSION__.py')
+
+with open(version_file, 'r') as fh:
+    version_tag = open(version_file).read().strip().split('=')[-1].replace('"','').replace("'", '').replace(" ", "")
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README')).read()
 CHANGES = open(os.path.join(here, 'CHANGELOG')).read()
 
+kwargs = {}
+
+kwargs['install_requires'] = [
+]
+
+kwargs['extras_require'] = {
+}
+
+kwargs['entry_points'] = {
+}
+
 setup(
     name='timecode',
-    version='0.3.0',
+    version=version_tag,
     description="SMPTE Time Code Manipulation Library",
     long_description='%s\n\n%s' % (README, CHANGES),
-    classifiers=[
-        "Programming Language :: Python",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Development Status :: 5 - Production/Stable",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    author=['Erkan Ozgur Yilmaz'],
-    author_email=['eoyilmaz@gmail.com'],
-    url='https://github.com/eoyilmaz/timecode',
+    author=['Erkan Ozgur Yilmaz', "Julien Brasseur"],
+    author_email=['eoyilmaz@gmail.com', "julien.brasseur@dwarf-labs.com"],
     keywords=['video', 'timecode', 'smpte'],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=True,
+    **kwargs
 )
